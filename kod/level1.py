36b5564a3afeb8bb2595c1dcc0766335d40553d4
jadie <jadeczjade008@gmail.com>
2025-11-14 15:24:52 +0100

    level1

---
+++ kod/level1.py
@@ -0,0 +1,21 @@
+import os
+
+
+def level1(level_name):
+    # gives the path of demo.py
+    path = os.path.realpath(__file__)
+
+    # gives the directory where demo.py
+    # exists
+    dir = os.path.dirname(path)
+    file = open(dir.replace("kod", "vstupy") + "/" + level_name, "r")
+    i = 0
+    for line in file:
+        if i == 0:
+            i += 1
+            continue
+        new_line = line.rsplit()
+        final = 0
+        for number in line.split(" "):
+            final += int(number)
+
